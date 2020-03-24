42a0bc9b9bf4a2880e6a6bc2dbb677c33e5c4be6
kantevski <kantevsk@pa.uc3m.es>
2020-03-24 17:00:21 +0100

    first commit

---
+++ kiril_web.js
@@ -0,0 +1,10 @@
+function expandContent(element) {
+    if(document.getElementById(element).style.display == "none"){
+        document.getElementById(element).style.display = "block"
+    }
+    else{
+        document.getElementById(element).style.display = "none";
+
+    }
+    // console.log(element)
+   }
